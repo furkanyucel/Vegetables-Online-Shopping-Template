9dc8ff9473f044a8cf22e5f6e3d65aa0cf239143
furkanyucel <furkanyucelux@gmail.com>
2023-12-02 21:44:28 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default {
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['@ant-design/icons'] // @ant-design/icons modülünü buraya ekleyin
